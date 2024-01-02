6f12d370fecbe663a6b008b4cf945b0f76f5e627
Alberto Garsan <albertogarsan@macbook-albertogarsan.technicolor.net>
2024-01-02 17:10:12 +0100

    demo finished

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,85 @@
+<!-- Els components tenen una part de template que dins té els elements que acabaran sent nodes del DOM -->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h1>Personas</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <!-- El @ és l'identificador de l'event del formulari que ha de coincidir amb el primer paràmetre de l'.$emit.-->
+        <formulario-persona @add-persona="agregarPersona" />
+        <!-- Ací tenim el component -->
+        <tabla-personas
+          :personas="personas"
+          @delete-persona="eliminarPersona"
+          @actualizar-persona="actualizarPersona"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
+<!-- I parts d'scripts que és on escriurem les funcions i objectes que vue utilitza per a funcionar -->
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TablaPersonas from "./components/TablaPersonas.vue";
+import FormularioPersona from "./components/FormularioPersona.vue";
 
 export default {
-  name: 'App',
+  name: "app",
   components: {
-    HelloWorld
-  }
-}
+    TablaPersonas,
+    FormularioPersona,
+  },
+  data() {
+    return {
+      personas: [
+        {
+          id: 1,
+          nombre: "Jon",
+          apellido: "Nieve",
+          email: "jon@email.com",
+        },
+        {
+          id: 2,
+          nombre: "Tyrion",
+          apellido: "Lannister",
+          email: "tyrion@email.com",
+        },
+        {
+          id: 3,
+          nombre: "Daenerys",
+          apellido: "Targaryen",
+          email: "daenerys@email.com",
+        },
+      ],
+    };
+  },
+  methods: {
+    agregarPersona(persona) {
+      /* this.personas = [...this.personas, persona]; */
+      const ides = this.personas.map((persona) => persona.id);
+      const idMax = Math.max(...ides);
+      persona.id = idMax + 1;
+      this.personas.push(persona);
+    },
+    eliminarPersona(id) {
+      this.personas = this.personas.filter((persona) => persona.id !== id);
+    },
+    actualizarPersona(id, personaActualizada) {
+      this.personas = this.personas.map((persona) =>
+        persona.id === id ? personaActualizada : persona
+      );
+    },
+  },
+};
 </script>
 
+<!-- I la part d'estil per a tindre-ho tot junt -->
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+button {
+  background: #009435;
+  border: 1px solid #009435;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+//Inicialitza tota la funcionalitat de vue
+import { createApp } from "vue";
+//Importem el template mare (component principal o root)
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+//Invoquem la funció createApp passant-li el component principal com a argument i això ens retorna la nostra app.
+const app = createApp(App);
+//Invoquem el mètode mount de la nostra app passant-li l'ID de l'HTML dins del qual es muntarà l'aplicació vue
+app.mount("#app");
